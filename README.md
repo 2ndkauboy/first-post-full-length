15175a9c03ef6471e6ad16e197a6685a1adb591e
2ndkauboy <bernhard@kau-boys.de>
2015-12-06 16:17:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# First Post Full Length #
+**Contributors:** Kau-Boy
+**Donate link:** https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7914504
+**Tags:** post, excerpt, more
+**Stable tag:** 0.1
+**License:** GPLv3
+**License URI:** http://www.gnu.org/licenses/gpl-3.0
+
+
+This plugin will show the first post with it's full text but all the others with the text after the "more tag"
+
+## Change Log ##
+
+### 0.1 ###
+* First stable release
+* Time invested for this release: 120min
---
+++ first-post-full-length.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name: First Post Full Length
+ * Description: Show the first post in full length if it has a more tag and the excerpt for the other posts
+ * Version: 1.0.0
+ * Author: Bernhard Kau
+ * Author URI: http://kau-boys.de
+ * Plugin URI: https://github.com/2ndkauboy/first-post-full-length
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0
+*/
+
+add_action( 'loop_start', 'first_post_full_length_enable' );
+
+/**
+ * @param WP_Query $query
+ */
+function first_post_full_length_enable( $query ) {
+
+	if ( ! is_admin() && $query->is_main_query() && ! $query->is_feed() ) {
+		// set is_feed to true, so that the post content will be used in full length
+		$query->is_feed = true;
+		// add a filter that sets is_feed to false again, as soon as the content from the first post was set
+		add_filter( 'the_post', 'first_post_full_length_disable' );
+	}
+}
+
+function first_post_full_length_disable( $args = array() ) {
+	global $wp_the_query;
+
+	if ( ! is_admin() && $wp_the_query->is_main_query() ) {
+		$wp_the_query->is_feed = false;
+	}
+}
